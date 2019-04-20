07af970fe56d562cbb9da5884b4f230bed6a21ff
sandy <sandytian2000@hotmail.com>
2019-04-19 20:28:12 -0400

    jwt and auth

--- src/index.js
+++ src/index.js
@@ -15,3 +15,15 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log('server is up on port ', port)
 });
+
+const jwt = require('jsonwebtoken');
+
+const myFunction = async () => {
+  const token = jwt.sign({_id: 'abc123'}, 'testmctestface', {expiresIn: '7 days'});
+  console.log(token)
+
+  const data = jwt.verify(token, 'testmctestface')
+  console.log(data)
+}
+
+myFunction()
