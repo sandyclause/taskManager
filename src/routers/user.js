f82a3f735bde555b08b71ed51d3b889b05cfd6b8
sandy <sandytian2000@hotmail.com>
2019-04-23 19:27:46 -0400

    login with token

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const router = new express.Router();
 const User = require('../models/user');
+const auth = require('../middleware/auth');
+const router = new express.Router();
 
 router.post('/users', async (req, res) => {
   const user = new User(req.body);
@@ -16,13 +17,16 @@ router.post('/users', async (req, res) => {
 
 router.post('/users/login', async (req, res) => {
   try {
-    
     const user = await User.findByCredentials(req.body.email, req.body.password);
     const token = await user.generateAuthToken();
     res.send({user, token})
   } catch (e) {
     res.status(400).send()
   }
+})
+
+router.get('/users/me', auth, async (req, res) => {
+  res.send(req.user)
 })
 
 router.get('/users/:id', async (req, res) => {
@@ -40,14 +44,6 @@ router.get('/users/:id', async (req, res) => {
   }
 })
 
-router.get('/users', async (req, res) => {
-  try {
-    const users = await User.find({})
-    res.send(users)
-  } catch (e) {
-    res.status(500).send();
-  }
-})
 
 router.patch('/users/:id', async (req, res) => {
   const updates = Object.keys(req.body);
