4395e0299eb30d6aa59e74bf24f997663744be53
sandy <sandytian2000@hotmail.com>
2019-05-07 19:23:02 -0400

    upload file path

--- src/index.js
+++ src/index.js
@@ -13,19 +13,3 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log('server is up on port ', port)
 });
-
-const Task = require('./models/task');
-const User = require('./models/user');
-
-const main = async () => {
-  // const task = await Task.findById('5cc8e17822f72144504fd78b');
-  // await task.populate('owner').execPopulate()
-  // console.log(task.owner)
-
-  const user = await User.findById('5cc8e0ea5beee8050897d4f4');
-  await user.populate('tasks').execPopulate();
-  console.log(user.tasks)
-}
-
-
-main()
